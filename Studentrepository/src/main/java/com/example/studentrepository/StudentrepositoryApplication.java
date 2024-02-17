14a5e4823333a82c6ce1c9b6275046c0ab915d18
BhargaviMupparaju <105601164+BhargaviMupparaju@users.noreply.github.com>
2024-02-17 09:40:13 -0500

    Init commit

---
+++ Studentrepository/src/main/java/com/example/studentrepository/StudentrepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentrepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentrepositoryApplication.class, args);
+    }
+
+}
